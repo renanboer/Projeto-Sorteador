8f81aa12d908cb8f2a2a5009d00e2ae0b67a1caf
renanboer <renan_boer@hotmail.com>
2024-06-12 18:21:09 -0300

    Projeto-Sorteador

---
+++ script.js
@@ -0,0 +1,14 @@
+function generateNumber() {
+
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+    if (max > min) {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min
+
+        alert(result)
+
+    } else {
+        alert("o Valor minimo tem que ser MENOR que o valor máximo")
+    }
+}
